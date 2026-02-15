26e32c05db936b1332b7d22171629c5db212e5b6
Auxiliauxi <auxiliauxi@gmail.com>
2026-02-15 17:33:03 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
   images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
-}
+    unoptimized: true
+  }
+};
 
-export default nextConfig
+export default nextConfig;
 
